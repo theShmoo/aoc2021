3da5f968ced9d1e04472b74e0a14874427d72838
David Pfahler <pfahler.david@gmail.com>
2021-12-01 06:32:52 +0100

    cleanup day 01

--- day01/main.hs
+++ day01/main.hs
@@ -5,22 +5,20 @@ import Data.List
 readInt :: String -> Int
 readInt str = read str :: Int
 
-solve1 :: [String] -> Int
+solve1 :: [Int] -> Int
 solve1 allLines = do
-  let ints = map readInt allLines
   let zipped = zip ints (drop 1 ints)
   sum [1 | (val, next) <- zipped, val < next]
 
-
-solve2 :: [String] -> Int
+solve2 :: [Int] -> Int
 solve2 allLines = do
-  let ints = map readInt allLines
   let tripples = transpose [ints, (drop 1 ints), (drop 2 ints)]
   let zipped = zip tripples (drop 1 tripples)
   sum [1 | (val, next) <- zipped, (sum val) < (sum next)]
 
 main = do
   content <- readFile "input.txt"
+  let ints = map readInt allLines
   let allLines = lines content
   print . solve1 $ allLines
   print . solve2 $ allLines
